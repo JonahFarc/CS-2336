8d4f8231b86ceecd8a801acb9dc8941950040692
JonahFarc <jonahfarc@gmail.com>
2016-09-26 14:29:12 -0500

    Formatting
    Formatted the files so they look less disgusting.

--- src/drinkmachine.h
+++ src/drinkmachine.h
@@ -9,25 +9,25 @@
 #include "receipt.h"
 class DrinkMachine
 {
-private:
-	unsigned int version;
-	unsigned int numDrinks;
-	DrinkItem drinks[25];
-	unsigned int maxDrinks;
-public:
-	DrinkMachine();
-	~DrinkMachine();
-	unsigned int size() const;
-	unsigned int max_size() const;
-	DrinkItem& at(unsigned int index);
-	const DrinkItem& at(unsigned int index) const;
-	bool available(unsigned int drinkId) const;
-	double getPrice(unsigned int drinkId) const;
-	Receipt purchase(unsigned int drinkId, double amount);
-	void addDrinks(unsigned int drinkId, unsigned int amount);
-	void print(std::ostream& outStream) const;
-	double sales() const;
-	double sales(unsigned int drinkId) const;
+	private:
+		unsigned int version;
+		unsigned int numDrinks;
+		DrinkItem drinks[25];
+		unsigned int maxDrinks;
+	public:
+		DrinkMachine();
+		~DrinkMachine();
+		unsigned int size() const;
+		unsigned int max_size() const;
+		DrinkItem& at(unsigned int index);
+		const DrinkItem& at(unsigned int index) const;
+		bool available(unsigned int drinkId);
+		double getPrice(unsigned int drinkId) const;
+		Receipt purchase(unsigned int drinkId, double amount);
+		void addDrinks(unsigned int drinkId, unsigned int amount);
+		void print(std::ostream& outStream) const;
+		double sales() const;
+		double sales(unsigned int drinkId) const;
 };
 
 #endif /* DRINKMACHINE_H_ */
